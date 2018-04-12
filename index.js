844959e68d2363aa9f9ef65ebd0de7d1edad57b2
markcarrmusic@gmail.com <markcarrmusic@gmail.com>
2018-04-12 17:34:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ function happyHolidays () {
 
 // Specific Person Holiday Greeting
 function happyHolidaysTo (name) {
-  return "Happy holidays, " + 
+  return "Happy holidays, " + name + "!"
 }
 
 // Specific Holiday and Specific Person
 function happyHolidayTo (holiday, name) {
-  return "Happy `${holiday}, ${name}`!"
+  return "Happy " + holiday + "," + name
 }
 
 // Holiday countdown
 function holidayCountdown (holiday, days) {
-  return "It's" + days + "until" + holiday!
+  return "It's" + days + "until" + holiday + "!"
 }
